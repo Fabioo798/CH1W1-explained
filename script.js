2324e7ddf5a059377f39c1b54cbb9430e40b8509
Fabio <fabiodn.798@gmail.com>
2023-01-17 12:36:50 +0100

    conflicto en cambiar mismo achivo

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // clear everything on the screen
 function clearScreen() {
-        document.getElementById("result").value = "";
-    }
-    
-    //display the values
-    function display(value) {
-        document.getElementById("result").value += value;
-    }
-    
-    //evaluates the expression and returns the final result
-    function calculate() {
-        let expression = document.getElementById("result").value;
-        let output = eval(expression);
-        document.getElementById("result").value = output;
-    }
+  document.getElementById("result").value = "";
+}
+
+//display the values
+function display(value) {
+  document.getElementById("result").value += value;
+}
+
+//evaluates the expression and returns the final result
+function calculate() {
+  let expression = document.getElementById("result").value;
+  let output = eval(expression);
+  document.getElementById("result").value = output;
+}
+// creating a commit in the main , cambio en la rama
